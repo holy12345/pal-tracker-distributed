84d296892aafad1fbd7c3403ccd084f37fa3d960
holy12345 <spring_holy@163.com>
2018-05-11 17:15:41 -0600

    aaa

--- applications/registration-server/src/main/java/io/pivotal/pal/tracker/registration/App.java
+++ applications/registration-server/src/main/java/io/pivotal/pal/tracker/registration/App.java
@@ -10,6 +10,8 @@ import org.springframework.security.oauth2.config.annotation.web.configuration.E
 import java.util.TimeZone;
 
 
+@EnableWebSecurity
+@EnableResourceServer
 @EnableEurekaClient
 @SpringBootApplication
 @ComponentScan({
@@ -17,10 +19,8 @@ import java.util.TimeZone;
     "io.pivotal.pal.tracker.restsupport",
     "io.pivotal.pal.tracker.projects",
     "io.pivotal.pal.tracker.users",
-        "io.pivotal.pal.tracker.registration"
+    "io.pivotal.pal.tracker.registration"
 })
-@EnableWebSecurity
-@EnableResourceServer
 public class App {
     public static void main(String[] args) {
         TimeZone.setDefault(TimeZone.getTimeZone("UTC"));
